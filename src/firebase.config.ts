f54a9dd54f3648eea3de57b30902ad9169abe5ec
WoodySchultze <woodyschultze@gmail.com>
2023-09-27 15:02:01 -0700

    Added tracking to firebase.config.ts after abstracting out the sensitive information (e.g. google api key)

---
+++ src/firebase.config.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// NOTICE: Be sure to abstract away any sensitive information via env variables
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+export const db = getFirestore();
